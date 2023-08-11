3573924ee95eca6df346079f7a418fc134728f06
yun900800@126.com <yun900800@126.com>
2023-08-11 12:47:29 +0800

    feature(response-layout) 增加一个宠物页面的响应式布局

--- packages/next-misc/pages/pets.js
+++ packages/next-misc/pages/pets.js
@@ -3,18 +3,21 @@ import Image from  'next/image';
 import img from '../public/1.jpg'
 function Pets() {
     return (
-    <div className={styles.pets_container}>
-        <Image src={img} placeholder='blur' alt="pet" width='280' height='420'/>
-        {
-            [1,2,3,4,5].map( path=>{
-                return (
-                    <div key={path}>
-                        <Image src={`/${path}.jpg`} alt="pet" width='280' height='420'/>
-                    </div>
-                )
-            })
-        }
-    </div>)
+        <>
+            <div className={styles.pets_container}>
+                {
+                    [1,2,3,4,5,5,4,3,2,1].map( path=>{
+                        return (
+                            <div key={path} className={styles.pets_item}>
+                                <Image src={`/${path}.jpg`} alt="pet" width='280' style={{width:'100%',objectFit:'cover'}} height='420'/>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <Image src={img} placeholder='blur' alt="pet" width='280' className={styles.small_pets_item} height='420'/>
+        </>
+    )
 }
 
 export default Pets;
